842f016090db2341f8a76facdc87cbde2d4f0f0d
Thanh Tuan <tuanvp2001@gmail.com>
2023-10-31 11:42:30 +0700

    hoan thien trang chu

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,78 @@ menuCloseBtn.onclick = function () {
   navLinks.style.left = "-100%";
 };
 
+function scrollMouse(classFirst, nameClassAdd) {
+  $(classFirst).addClass("hidden");
+  $(classFirst).each(function () {
+    var spaceTop = $(this).offset().top;
+    var chieucao = $(this).height();
+    var spaceBottom = $(document).height() - $(this).offset().top - chieucao;
+    var scrollBottom =
+      $(document).height() + $(window).height() - $(window).scrollTop();
+    var scrollSpaceTop = $(window).scrollTop();
+    if (
+      spaceBottom < scrollBottom - $(window).height() &&
+      spaceTop < scrollSpaceTop + $(window).height()
+    ) {
+      $(this).addClass(nameClassAdd);
+    } else {
+      $(this).removeClass(nameClassAdd);
+    }
+  });
+}
+function scrollMouse1(classFirst, nameClassAdd) {
+  $(classFirst).addClass("hidden");
+  $(classFirst).each(function () {
+    var spaceTop = $(this).offset().top;
+    var chieucao = $(this).height();
+    var spaceBottom = $(document).height() - $(this).offset().top - chieucao;
+    var scrollBottom =
+      $(document).height() + $(window).height() - $(window).scrollTop();
+    var scrollSpaceTop = $(window).scrollTop();
+    if (
+      spaceBottom < scrollBottom - $(window).height() &&
+      spaceTop < scrollSpaceTop + $(window).height()
+    ) {
+      $(this).addClass(nameClassAdd);
+    }
+  });
+}
+var width = $(window).width();
+
+if (width > 767) {
+  $(window).scroll(function () {
+    scrollMouse1(".sds-featured-box", "slide-right");
+    scrollMouse1(".connection-part", "slide-left");
+  });
+}
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+$(document).ready(function () {
+  const count11 = document.getElementById("count11");
+  const count15 = document.getElementById("count15");
+  const count200k = document.getElementById("count200k");
+  const count200 = document.getElementById("count200");
+  animateValue(count11, 0, 11, 2000);
+  animateValue(count15, 0, 15, 2000);
+  animateValue(count200k, 0, 200000, 2000);
+  animateValue(count200, 0, 200, 2000);
+
+  setTimeout(function () {
+    $(count200k).html("200.000");
+  }, 2100);
+});
+
 // swiper element
 const swiperEl = document.querySelector("swiper-container");
 
